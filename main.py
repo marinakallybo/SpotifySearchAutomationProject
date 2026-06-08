5d618cb0830b933d8b556a06a4dd626b03e4e2be
Marina Kally <marinakally@gmail.com>
2026-06-08 16:42:07 -0300

    feat: Criando main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import pyautogui
+import time
+
+pyautogui.PAUSE = 0.5
+
+# Abrir o spotify
+pyautogui.press("win") # clicar com o mouse
+pyautogui.write("spotify")
+pyautogui.press("enter")
+
+time.sleep(5)
+
+pyautogui.hotkey('ctrl', 'l')
+pyautogui.write("marina sena - desmitificar")
+
+time.sleep(3)
+pyautogui.press("enter")
