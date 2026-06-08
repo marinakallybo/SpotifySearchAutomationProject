b75c8a1ce432f8f8278244b9cec1e0ccb21a8c2c
Marina Kally <marinakally@gmail.com>
2026-06-08 16:49:56 -0300

    feat: Implementando busca com o click do cursor

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ pyautogui.press("win") # clicar com o mouse
 pyautogui.write("spotify")
 pyautogui.press("enter")
 
-time.sleep(5)
+time.sleep(6)
 
 pyautogui.hotkey('ctrl', 'l')
 pyautogui.write("marina sena - desmitificar")
 
 time.sleep(3)
 pyautogui.press("enter")
+
+time.sleep(3)
+pyautogui.click(x=1055, y=319)
